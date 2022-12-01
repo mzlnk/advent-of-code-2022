e03822698955012871c2e53451301d06d5f76962
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-01 20:21:39 +0100

    ✨ [Day-01] added solution for part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var current = 0
+
+        for(line: String in input) {
+            if(line.isEmpty()) {
+                max = Math.max(max, current)
+                current = 0
+                continue
+            }
+            current += line.toInt()
+        }
+        max = Math.max(max, current)
+
+        return max
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +22,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
