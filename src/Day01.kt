aeb60195205ea4401d32535c887d0c9e119f6494
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-01 20:30:35 +0100

    ✨ [Day-01] added solution for part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -17,12 +17,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = ArrayList<Int>()
+
+        var current = 0
+        for(line: String in input) {
+            if(line.isEmpty()) {
+                calories.add(current)
+                current = 0
+                continue
+            }
+            current += line.toInt()
+        }
+        calories.add(current)
+
+        return calories.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24_000)
+    check(part2(testInput) == 45_000)
 
     val input = readInput("Day01")
     println(part1(input))
