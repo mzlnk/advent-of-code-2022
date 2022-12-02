c95f0f5789cb85ca0d7c35ae2b21aadf6be49ab8
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-02 23:25:04 +0100

    ✨ [Day-02] added solution for part 1

---
+++ src/Day02.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val strategies = mapOf(
+            "A" to mapOf(
+                "X" to 4,
+                "Y" to 8,
+                "Z" to 3
+            ),
+            "B" to mapOf(
+                "X" to 1,
+                "Y" to 5,
+                "Z" to 9
+            ),
+            "C" to mapOf(
+                "X" to 7,
+                "Y" to 2,
+                "Z" to 6
+            )
+        )
+
+        var score = 0
+        for(line: String in input) {
+            val data = line.split(' ')
+            val opponent = data[0]
+            val you = data[1]
+
+            score += strategies[opponent]?.get(you) ?: 0
+        }
+
+        return score
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
