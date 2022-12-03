c5143bcb2140ccbdb2a70fa6da0507afbd73ef42
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-03 23:36:55 +0100

    ✨ [Day-03] added solution for part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val letterToPriority: (Char) -> Int = { letter -> (if(letter in 'a'..'z') (letter.code - 97) else (letter.code - 39)) + 1 }
+
+        var sum = 0
+        for((index: Int, line: String) in input.withIndex()) {
+            val pack1 = HashSet<Char>()
+
+            for(i in 0 until line.length / 2) {
+                pack1.add(line[i])
+            }
+
+            for(i in (line.length / 2) until line.length) {
+                if(pack1.contains(line[i])) {
+                    sum += letterToPriority(line[i])
+                    break;
+                }
+            }
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,10 +27,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 157)
     check(part2(testInput) == 0)
 
     val input = readInput("Day03")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
