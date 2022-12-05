aa551cf69e13e1f98f8a14c5208e8630ac44c438
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-05 23:36:20 +0100

    ✨ [Day-05] added solution for part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -36,14 +36,50 @@ fun main() {
         return stacks.map { it.pop() }.joinTo(buffer = StringBuilder(), separator = "").toString()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    // FIXME: refactor it
+    fun part2(input: List<String>): String {
+        val printStacks: (ArrayList<ArrayList<Char>>) -> Unit = { stacks -> for((index: Int, stack: ArrayList<Char>) in stacks.withIndex()) { println("Stack $index: $stack") }}
+
+        val stackLines = input.takeWhile { it[1] != '1' }.reversed()
+        val numbersLine = input[stackLines.size]
+        val instructionLines = input.subList(stackLines.size + 2, input.size)
+
+        val stacksSize = numbersLine.split(' ').last().toInt()
+
+        val stacks = ArrayList<ArrayList<Char>>()
+        for (idx: Int in 0 until stacksSize) {
+            stacks.add(ArrayList())
+        }
+
+        for (stackLine: String in stackLines) {
+            for ((stackIdx, offset) in (0..stacks.size).zip(1 until stackLine.length step 4)) {
+                val letter = stackLine[offset]
+                if (letter.isLetter()) {
+                    stacks[stackIdx].add(stackLine[offset])
+                }
+            }
+        }
+
+        for (instruction: String in instructionLines) {
+            val pattern = Regex("move (\\d+) from (\\d+) to (\\d+)")
+            val (amount, from, to) = pattern.find(instruction)!!.destructured
+
+            val taken = stacks[from.toInt() - 1].takeLast(amount.toInt())
+            printStacks(stacks)
+            for(i in taken.indices) {
+                stacks[from.toInt() - 1].removeLast()
+            }
+            stacks[to.toInt() - 1].addAll(taken)
+            printStacks(stacks)
+        }
+
+        return stacks.map { it.last() }.joinTo(buffer = StringBuilder(), separator = "").toString()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == "CMZ")
-    check(part2(testInput) == 0)
+    check(part2(testInput) == "MCD")
 
     val input = readInput("Day05")
     println(part1(input))
