7262619274d8f74f7407936b1f9b2ade2aae7be2
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-12-05 23:06:48 +0100

    ✨ [Day-05] added solution for part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,39 @@
+import java.lang.StringBuilder
+import java.util.Stack
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): String {
+        val stackLines = input.takeWhile { it[1] != '1' }.reversed()
+        val numbersLine = input[stackLines.size]
+        val instructionLines = input.subList(stackLines.size + 2, input.size)
+
+        val stacksSize = numbersLine.split(' ').last().toInt()
+
+        val stacks = ArrayList<Stack<Char>>()
+        for (idx: Int in 0 until stacksSize) {
+            stacks.add(Stack())
+        }
+
+        for (stackLine: String in stackLines) {
+            for ((stackIdx, offset) in (0..stacks.size).zip(1 until stackLine.length step 4)) {
+                val letter = stackLine[offset]
+                if (letter.isLetter()) {
+                    stacks[stackIdx].push(stackLine[offset])
+                }
+            }
+        }
+
+        for (instruction: String in instructionLines) {
+            val pattern = Regex("move (\\d+) from (\\d+) to (\\d+)")
+            val (amount, from, to) = pattern.find(instruction)!!.destructured
+
+            for (i: Int in 0 until amount.toInt()) {
+                val popped = stacks[from.toInt() - 1].pop()
+                stacks[to.toInt() - 1].push(popped)
+            }
+        }
+
+        return stacks.map { it.pop() }.joinTo(buffer = StringBuilder(), separator = "").toString()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +42,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == "CMZ")
     check(part2(testInput) == 0)
 
     val input = readInput("Day05")
