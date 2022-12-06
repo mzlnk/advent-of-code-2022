09f71b3ee3945793c6cafe9de49efb0fab0545ca
Marcin Zielonka <marcin.zielonka@tesco.com>
2022-12-06 09:41:35 +0100

    ✨ [Day-06] added solution for part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -17,7 +17,20 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val line = input.first()
+
+        val checker = HashSet<Char>()
+        for (i in 0 until (line.length - 13)) {
+            for (j in 0..13) {
+                checker.add(line[i + j])
+            }
+
+            if (checker.size == 14) {
+                return i + 14
+            }
+            checker.clear()
+        }
+        return -1
     }
 
     // test if implementation meets criteria from the description, like:
@@ -31,10 +44,10 @@ fun main() {
     check(part1(testInput3) == 10)
     check(part1(testInput4) == 11)
 
-    check(part2(testInput1) == 0)
-    check(part2(testInput2) == 0)
-    check(part2(testInput3) == 0)
-    check(part2(testInput4) == 0)
+    check(part2(testInput1) == 23)
+    check(part2(testInput2) == 23)
+    check(part2(testInput3) == 29)
+    check(part2(testInput4) == 26)
 
     val input = readInput("Day06")
     println(part1(input))
